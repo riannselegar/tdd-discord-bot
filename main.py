57ecdbbee09b51622959d734ddb6ee355bda608a
riannselegar <riannselegar@gmail.com>
2021-09-12 22:08:36 -0300

    Log de entrada e saída

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from datetime import datetime
 
 client = discord.Client()
 token = os.environ['TOKEN']
@@ -7,14 +8,32 @@ token = os.environ['TOKEN']
 
 @client.event
 async def on_ready():
-  print("we have logged in as {0.user}".format(client))
+    print("Logged in. Listening... {0.user}".format(client))
+
 
 @client.event
 async def on_message(msg):
-  if msg.author == client.user:
-    return
-  
-  if msg.content.startswith('$hello'):
-    await msg.channel.send("Hello!")
+    if msg.author == client.user:
+        return
+
+    if msg.content.startswith('$hello'):
+        await msg.channel.send("Hello novo!")
+
+
+@client.event
+async def on_typing(channel, user, when):
+    print(channel, user, when)
+
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None or after.channel is None:
+        print("O usuário {user} {acao} {servidor} às {hora}"
+              .format(
+                user=member,
+                acao="saiu do" if after.channel is None else "entrou no",
+                servidor=before.channel.guild if after.channel is None else after.channel.guild,
+                hora=datetime.now().strftime("%H:%M:%S")))
+
 
 client.run(token)
