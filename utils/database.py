b6c888b8ded4670f0f108f7ae34772cc5a9d254e
riannselegar <riannselegar@gmail.com>
2021-09-15 20:56:26 -0300

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -3,8 +3,8 @@ import os
 import certifi
 from pymongo import MongoClient
 
-connection_string = os.environ["CONNECTION_STRING"]
+_connection_string = os.environ["CONNECTION_STRING"]
 
-cluster = MongoClient(connection_string, tlsCAFile=certifi.where())
+cluster = MongoClient(_connection_string, tlsCAFile=certifi.where())
 db = cluster["TDD"]
 
